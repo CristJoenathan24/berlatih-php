37243642c0bc1d643c75bd26685426edd1d9ac27
CristJoenathan24 <crist.joenathan8@gmail.com>
2020-07-29 17:28:38 +0700

    palindrome-angka DONE

--- palindrome-angka.php
+++ palindrome-angka.php
@@ -1,1 +1,41 @@
-asdasd
+<?php
+
+function palindrome_angka($angka) {
+    $a = $angka;
+    
+    if($angka >= 0 && $angka<10){
+        echo $angka+1 . "<br>";
+    }else{
+        while(true){
+            $strOri = strval($a);
+            $str = strval($a);
+            // echo $a . "<br>";
+            for($i = strlen($str)-1, $j = 0; $j < $i ; $i-- , $j++ ){
+                $temp = $str[$i];
+                $str[$i] = $str[$j];
+                $str[$j] = $temp;
+            }
+            // echo $str;
+            // echo $str . "<br>";
+            $buff = strcmp($strOri,$str);
+            // echo $buff . "<br>";
+
+            if($buff == 0){
+                echo $strOri . "<br>";
+                break;
+            }else{
+                $a++;
+            }
+        }
+
+    }
+}
+
+// TEST CASES
+echo palindrome_angka(8); // 9
+echo palindrome_angka(10); // 11
+echo palindrome_angka(117); // 121
+echo palindrome_angka(175); // 181
+echo palindrome_angka(1000); // 1001
+
+?>
